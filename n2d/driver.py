f4ae84f1368eff50ac3de584128e019c3c6fec75
fanzou2020 <fanzou2020@icloud.com>
2020-04-02 17:17:29 -0400

    add nfa to dfa test2

--- n2d/driver.py
+++ n2d/driver.py
@@ -17,3 +17,17 @@ nfa = automata_IO.nfa_dot_importer("nfa_transition_table.dot")
 dfa = NFA.nfa_determinization(nfa)
 automata_IO.dfa_to_dot(dfa, "dfa_pysimpleautoma")
 
+
+# draw diagram of nfa
+tb_to_diagram.nfa_tb_to_diagram("nfa_test_tb.json", "nfa_test_tb")
+
+# transform nfa to dfa
+nfa_to_dfa.nfa_to_dfa("nfa_test_tb.json", "dfa_test_tb.json")
+
+# draw diagram of dfa
+tb_to_diagram.dfa_tb_to_diagram("dfa_test_tb.json", "dfa_test_tb")
+
+# use PySimpleAutomata to check correctness
+nfa = automata_IO.nfa_dot_importer("nfa_test_tb.dot")
+dfa = NFA.nfa_determinization(nfa)
+automata_IO.dfa_to_dot(dfa, "dfa_pysimpleautoma_test")
