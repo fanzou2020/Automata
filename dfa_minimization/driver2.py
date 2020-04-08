0b7deaf9fdb6e7e2dd40dd6d60d0fac043c10346
fanzou2020 <fanzou2020@icloud.com>
2020-04-08 15:36:50 -0400

    bug fix

--- dfa_minimization/driver2.py
+++ dfa_minimization/driver2.py
@@ -3,4 +3,6 @@ from draw_diagram import tb_to_diagram
 
 DFA_min.dfa_minimization("dfa_before_2.json", "dfa_after_2.json")
 
+tb_to_diagram.dfa_tb_to_diagram("dfa_before_2.json", "dfa_before_2")
+
 tb_to_diagram.dfa_tb_to_diagram("dfa_after_2.json", "dfa_after_2")
